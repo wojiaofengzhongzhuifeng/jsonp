a9c7601bdc8e3065cad3360e88978e37118a71cd
饶家俊 <471412450@qq.com>
2021-05-14 11:10:47 +0800

    封装 JSONP

--- server/index.js
+++ server/index.js
@@ -11,7 +11,7 @@ const server = http.createServer((req, res) => {
       name: 'datafromserver-raojiajun',
       id: params.id
     }
-    // 如果存在 callback pramas 说明需要使用 JSONP 获取数据,后端应该拼接 test1223321({name: 'datafromserver-raojiajun', id: 123321}) 字符串
+    // 后端已经定义了 params 的字段必须为 callback, 前端需要问后端 callback
     if(params.callback){
       let resData = `${params.callback}(${JSON.stringify(mockData)})`
       res.end(resData)
