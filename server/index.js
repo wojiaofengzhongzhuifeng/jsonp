e5992eb81e21627f16488c5ac27d91aacb5be7d3
饶家俊 <471412450@qq.com>
2021-05-14 09:58:29 +0800

    完成后端接口

--- server/index.js
+++ server/index.js
@@ -3,12 +3,8 @@ const url = require('url')
 const qs = require('querystring')
 
 const server = http.createServer((req, res) => {
-
-
   const { pathname, query } = url.parse(req.url)
-
   const params = qs.parse(query)
-
 
   if(pathname === '/api/user'){
     let mockData = {
@@ -17,20 +13,6 @@ const server = http.createServer((req, res) => {
     }
     res.end(JSON.stringify(mockData))
   }
-
-  if(req.url){}
-
-
-
-  const data = { name: 'shanyue', id: params.id }
-
-  if (params.callback) {
-    str = `${params.callback}(${JSON.stringify(data)})`
-    res.end(str)
-  } else {
-    res.end()
-  }
-
 })
 
 server.listen(10010, () => console.log('Done'))
